5dd232771ec24752552d42f8f9724e59032c0040
li.pengjie_202380 <li.pengjie_202380@immomo.com>
2021-12-22 19:21:34 +0800

    push

--- script.js
+++ script.js
@@ -1,215 +1,161 @@
-MorphSVGPlugin.convertToPath('polygon');
-var xmlns = "http://www.w3.org/2000/svg",
-  xlinkns = "http://www.w3.org/1999/xlink"
-select = function(s) {
-    return document.querySelector(s);
-  },
-  selectAll = function(s) {
-    return document.querySelectorAll(s);
-  },
-  pContainer = select('.pContainer'),
-  mainSVG = select('.mainSVG'),
-  star = select('#star'),
-  sparkle = select('.sparkle'),
-  tree = select('#tree'),
-  showParticle = true,
-  //particleColorArray = ['#69D2E7', '#A7DBD8', '#E0E4CC', '#F38630', '#FA6900'],
-  //particleColorArray = ['#7FBBEF','#D5780F','#381502','#FDED56','#7D1607','#F0C495','#C2293B','#53AB61','#343DE1','#FCD422'],
-  //particleColorArray = ['#FFFCF9', '#339B8B', '#B74551','#C89568','#CC9966', '#336666', '#669966', '#CC9966', '#CC3333', '#339B8B', '#B74551'],
-  particleColorArray = ['#E8F6F8', '#ACE8F8', '#F6FBFE','#A2CBDC','#B74551', '#5DBA72', '#910B28', '#910B28', '#446D39'],
-  particleTypeArray = ['#star','#circ','#cross','#heart'],
-  particleTypeArray = ['#star'],
-  particlePool = [],
-  particleCount = 0,
-  numParticles = 200
+var bbiTL = new TimelineMax(),
+		
+// logo
+		frame = document.getElementById("frame"),
+		happy = document.getElementById("happy"),
+		merry = document.getElementById("merry"),
+		christmas = document.getElementById("christmas"),
+		trees = document.getElementById("trees"),
+		middle_tree = document.getElementById("middle_tree"),
+		left_tree = document.getElementById("left_tree"),
+		right_tree = document.getElementById("right_tree");
+
+// animations
+		// logo
+		bbiTL
+			.fromTo(merry, 1, { scale: 0, xPercent: 50, yPercent: 100 }, { scale: 1, ease: Back.easeOut.config(1.8), xPercent: 0, yPercent: 0}, 1.5)
+			.fromTo(christmas, 1.5, { scale: 0, xPercent: 50, yPercent: 100 }, { scale: 1, ease: Bounce.easeOut, xPercent: 0, yPercent: 0}, 2)
+			.fromTo(frame, 1, { opacity: 0, scale: 0, xPercent: 50, yPercent: 100 }, { scale: 1, ease: Back.easeOut.config(1.8), xPercent: 0, yPercent: 0, opacity: 1}, 3)
+			.fromTo(middle_tree, 0.6, { opacity: 0, scale: 0, xPercent: 50, yPercent: 100 }, { scale: 1, ease: Back.easeOut.config(2), xPercent: 0, yPercent: 0, opacity: 1}, 6.9)
+			.fromTo(left_tree, 0.6, { opacity: 0, scale: 0, xPercent: 50, yPercent: 100 }, { scale: 1, ease: Back.easeOut.config(2), xPercent: 0, yPercent: 0, opacity: 1}, 6)
+			.fromTo(right_tree, 0.6, { opacity: 0, scale: 0, xPercent: 50, yPercent: 100 }, { scale: 1, ease: Back.easeOut.config(2), xPercent: 0, yPercent: 0, opacity: 1}, 6.3)
+			.fromTo(bbi, 1.5, { scale: 0, xPercent: 50, yPercent: 100 }, { scale: 1, ease: Bounce.easeOut, xPercent: 0, yPercent: 0}, 7.5)
+
+			happyNewYear();
+		
+		// item drop
+		var totalItems = 18;
+		for (var i=1; i <= totalItems; ++i) {
+			var lenght = Math.random() * (4.5 - 3) + 3;
+			var start = Math.random();
+			
+			// hanging
+			hanging(totalItems,i,lenght,start);
+			
+			bbiTL.fromTo("#item"+i, lenght, {y: -(($("#item"+i).height())/3) }, { ease: Bounce.easeOut, y: 0 }, start)
+		
+		};
+
+		// item hanging
+
+		function hanging(totalItems,i,lenght,start) {
+			var hangOffset = 0.3;
+			var hangStart = start+lenght-0.2;
+			var delay = (Math.random() * 3) + 1;
+			var rotation = -(( 1 / lenght ) * 3);
+			bbiTL.to("#item"+i, hangOffset, {rotation: rotation, transformOrigin: "0% 0%", repeatDelay:0, ease: Back.easeOut.config(2), repeat:-1 }, hangStart/3);
+    	bbiTL.to("#item"+i, 10, {rotation: 0, transformOrigin: "0% 0%", ease: Elastic.easeOut.config(2.5, 0.1), repeatDelay:hangOffset, repeat:-1 }, (hangStart+hangOffset)/3);
+			console.log(rotation);
+		}
 
 
-TweenMax.set('svg', {
-  visibility: 'visible'
-})
-
-TweenMax.set(sparkle, {
-	transformOrigin:'50% 50%',
-	y:-100
-})
-
-var treePath = MorphSVGPlugin.pathDataToBezier(select('.treePath').getAttribute('d'), {
-  offsetX: 0,
-  offsetY: -5
-});
-var treeBottomPath = MorphSVGPlugin.pathDataToBezier(select('.treeBottomPath').getAttribute('d'), {
-  offsetX: 0,
-  offsetY: -5
-});
-
-//console.log(starPath.length)
-var mainTl = new TimelineMax({delay:2, repeat:-1}), starTl;
+		function happyNewYear() {
+			for (var h=1; h <= 16; ++h) {
+				var leters = h*0.1;
+				bbiTL.fromTo(".happy_"+h, 0.2, { scale: -1, opacity: 0 }, { scale: 1, ease: Back.easeOut.config(1.4), opacity: 1 }, leters+4);
+			}
+		}
 
 
-//tl.seek(100).timeScale(1.82)
+		// onhover
+// 		$(".item").hover(itemhover(i));
+		
+// 		function itemhover(i) {
+// 			bbiTL.to("#item"+i, 0.3, {rotation: -0.8, transformOrigin: "0% 0%", }, 0);
+//     	bbiTL.to("#item"+i, 4, {rotation: 0, transformOrigin: "0% 0%", ease: Elastic.easeOut.config(2.5, 0.1) }, 0.3);
+// 		}
 
-function randomBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+		// snow
+		var canvas = document.getElementById('snow'),
+				ctx = canvas.getContext('2d'),
+				width = ctx.canvas.width = canvas.offsetWidth,
+				height = ctx.canvas.height = canvas.offsetHeight,
+				animFrame = window.requestAnimationFrame ||
+										window.mozRequestAnimationFrame ||
+										window.webkitRequestAnimationFrame ||
+										window.msRequestAnimationFrame,
+				snowflakes = [];
 
-function flicker(p){
-  
-  //console.log("flivker")
-  TweenMax.killTweensOf(p, {opacity:true});
-  TweenMax.fromTo(p, 0.07,{
-    alpha:1
-  }, {
-    alpha:Math.random(),
-    //ease: RoughEase.ease.config({ template: Power0.easeNone, strength: 3, points: 6, taper: "both", randomize: true, clamp: false}),
-    repeat:-1
-  })
-}
+		window.onresize = function() {
+			width = ctx.canvas.width = canvas.offsetWidth;
+			height = ctx.canvas.height = canvas.offsetHeight;
 
-function createParticles() {
-  
-  //var step = numParticles/starPath.length;
-  //console.log(starPath.length)
-  var i = numParticles, p, particleTl, step = numParticles/treePath.length, pos;
-  while (--i > -1) {
-    
-    p = select(particleTypeArray[i%particleTypeArray.length]).cloneNode(true);
-    mainSVG.appendChild(p);
-    p.setAttribute('fill', particleColorArray[i % particleColorArray.length]);
-    p.setAttribute('class', "particle");   
-    particlePool.push(p);
-    //hide them initially
-    TweenMax.set(p, {
-                 x:-100, 
-                 y:-100,
-   transformOrigin:'50% 50%'
-                 })
-    
-    
+			for (var i = 0; i < snowflakes.length; i++) {
+				snowflakes[i].resized();
+			}
+		}
 
-  }
+		function update() {
+			for (var i = 0; i < snowflakes.length; i++) {
+				snowflakes[i].update();
+			}
+		}
 
-}
-function getScale(){
-  return randomBetween(5, 20)/10;
-}
-function playParticle(p){
-  if(!showParticle){return};
-  var p = particlePool[particleCount]
- TweenMax.set(p, {
-      //attr:{
-          x: pContainer._gsTransform.x,
-          y: pContainer._gsTransform.y,
-          scale:getScale()
-      //}
-    }
-    );
-var tl = new TimelineMax();
-  tl.to(p, randomBetween(10, 60)/10, {
-      //paused:true,
-      physics2D: {
-        velocity: randomBetween(-13, 16),
-        angle:randomBetween(-180, 180),
-        gravity:randomBetween(-13, 44)
-      },
+		function Snow() {
+			this.x = random(0, width);
+			this.y = random(-height, 0);
+			this.radius = random(0.5, 3.0);
+			this.speed = random(0.5, 2.0);
+			this.wind = random(-0.1, 1.0);
+			this.isResized = false;
 
-      scale:0,
-      rotation:randomBetween(-123,360),
-      //skewY:(Math.random() * 180),
-      ease: Power1.easeOut,
-      onStart:flicker,
-      onStartParams:[p],
-      //repeat:-1,
-      onRepeat: function(p) {        
-        TweenMax.set(p, {         
-            scale:getScale()
-        })
+			this.updateData = function () {
+				this.x = random(0, width);
+				this.y = random(-height, 0);
+			}
 
-      },
-      onRepeatParams: [p]
+			this.resized = function () {
+				this.isResized = true;
+			}
+		}
 
-    });
-  
+		Snow.prototype.draw = function() {
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+			ctx.fillStyle = '#fff';
+			ctx.fill();
+			ctx.closePath();
+		}
 
-  //
-  //particlePool[particleCount].play();
-  particleCount++;
-  //mainTl.add(tl, i / 1.3)
-  particleCount = (particleCount >=numParticles) ? 0 : particleCount
-  
-}
+		Snow.prototype.update = function() {
+			this.y += this.speed;
+			this.x += this.wind;
 
-function drawStar(){
-  
-  starTl = new TimelineMax({onUpdate:playParticle})
-  starTl.to([pContainer, sparkle], 6, {
-    bezier: {
-      type: "cubic",
-      values: treePath,
-      autoRotate: false
-    },
-    ease: Linear.easeNone
-  })  
-  .to([pContainer, sparkle], 1, {
-    onStart:function(){showParticle = false},
-    x:treeBottomPath[0].x,
-    y:treeBottomPath[0].y
-  })
-  .to([pContainer, sparkle], 2, {
-    onStart:function(){showParticle = true},
-    bezier: {
-      type: "cubic",
-      values: treeBottomPath,
-      autoRotate: false
-    },
-    ease: Linear.easeNone    
-  },'-=0')
-.from('.treeBottomMask', 2, {
-  drawSVG:'0% 0%',
-  stroke:'#FFF',
-  ease:Linear.easeNone
-},'-=2')  
-  
+			if (this.y > ctx.canvas.height) {
+				if (this.isResized) {
+					this.updateData();
+					this.isResized = false;
+				} else {
+					this.y = 0;
+					this.x = random(0, width); 
+				}
+			}
+		}
 
-  //TweenMax.staggerTo(particlePool, 2, {})
-  
-}
+		function createSnow(count) {
+			for (var i = 0; i < count; i++) {
+				snowflakes[i] = new Snow();
+			}
+		}
 
-function textAnim(){
-  
-}
+		function draw() {
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+			for (var i = 0; i < snowflakes.length; i++) {
+				snowflakes[i].draw();
+			}
+		}
 
-createParticles();
-drawStar();
-//ScrubGSAPTimeline(mainTl)
+		function loop() {
+			draw();
+			update();
+			animFrame(loop);
+		}
 
-mainTl.staggerFrom(['.treePathMask','.treePotMask','.treeBottomMask'], 6, {
-  drawSVG:'0% 0%',
-  stroke:'#FFF',
-  cycle:{
-    duration:[6, 1,2]
-  },
-  ease:Linear.easeNone
-},6)
-.from('.treeStar', 3, {
-  //skewY:270,
-  scaleY:0,
-  scaleX:0.15,
-  transformOrigin:'50% 50%',
-  ease:Elastic.easeOut.config(1,0.5)
-},'-=4')
-  .to(['.treeStarOutline', '.messageText'], 1, {
-    alpha:1,
-    ease:RoughEase.ease.config({ template:  Power0.easeNone, strength: 2, points: 16, taper: "none", randomize: true, clamp: false})
-  },'-=3')
- .to(sparkle, 3, {
-    alpha:0,
-    ease:RoughEase.ease.config({ template:  Power0.easeNone, strength: 2, points: 100, taper: "both", randomize: true, clamp: false})
-  },'-=5')
-.to('.whole', 2, {
-	alpha:0,
-	ease:Sine.easeIn
-},'+=2')
+		function random(min, max) {
+			var rand = (min + Math.random() * (max - min)).toFixed(1);
+			rand = Math.round(rand);
+			return rand;
+		}
 
-mainTl.add(starTl, 0)
-TweenMax.globalTimeScale(1.5);
-//ScrubGSAPTimeline(mainTl)
+		createSnow(200);
+		loop();
